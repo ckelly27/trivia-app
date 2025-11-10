53a01cb238e26efd5e8e34ad79d2ab5c924b6c5e
ckelly427 <cameron.kelly04@icloud.com>
2025-11-10 14:10:13 -0600

    tab changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,15 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Play',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Highscores',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="trophy.fill" color={color} />,
         }}
       />
     </Tabs>
